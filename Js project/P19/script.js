b22b00d7567574ceaaab464f5d262082a0d40f84
Siano <117085166+Sianooo@users.noreply.github.com>
2025-05-08 23:10:34 +0200

    Add files via upload

--- Js project/P19/script.js
+++ Js project/P19/script.js
@@ -2,17 +2,17 @@ document.addEventListener('DOMContentLoaded',()=>{
     const buttons=document.querySelectorAll('.btn');
 
     buttons.forEach(button=>{
-        button.addEventListener('click', event=>{
+        button.addEventListener('click',event=>{
             const ripple=document.createElement('span');
             ripple.classList.add('ripple');
 
             const rect=button.getBoundingClientRect();
-            const size=Math.max(rect.width, rect.height);
+            const size=Math.max(rect.width,rect.height);
             ripple.style.width=`${size}px`;
             ripple.style.height=`${size}px`;
 
-            const x=event.clientX - rect.left - size / 2;
-            const y=event.clientY - rect.top - size / 2;
+            const x=event.clientX-rect.left-size/2;
+            const y=event.clientY-rect.top-size/2;
             ripple.style.left=`${x}px`;
             ripple.style.top=`${y}px`;
 
