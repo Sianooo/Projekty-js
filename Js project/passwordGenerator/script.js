6e819ebe48428e332088879905839a6e9f99da01
Siano <117085166+Sianooo@users.noreply.github.com>
2025-09-09 22:43:56 +0200

    Add files via upload

---
+++ Js project/passwordGenerator/script.js
@@ -0,0 +1,39 @@
+const passwordDisplay = document.querySelector('#passwordDisplay');
+const lengthInput = document.querySelector('#lengthInput');
+const uppercaseCheckbox = document.querySelector('#uppercaseCheckbox');
+const lowercaseCheckbox = document.querySelector('#lowercaseCheckbox');
+const numbersCheckbox = document.querySelector('#numbersCheckbox');
+const symbolsCheckbox = document.querySelector('#symbolsCheckbox');
+const generateButton = document.querySelector('#generateBtn');
+
+generateButton.addEventListener('click', () => {
+    const length = lengthInput.value;
+    const includeUppercase = uppercaseCheckbox.checked;
+    const includeLowercase = lowercaseCheckbox.checked;
+    const includeNumbers = numbersCheckbox.checked;
+    const includeSymbols = symbolsCheckbox.checked;
+
+    const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    const numberChars = '0123456789';
+    const symbolChars = '!@#$%^&*()_+[]{}|;:,.<>?';
+
+    let allChars = '';
+    if (includeUppercase) allChars += uppercaseChars;
+    if (includeLowercase) allChars += lowercaseChars;
+    if (includeNumbers) allChars += numberChars;
+    if (includeSymbols) allChars += symbolChars;
+
+    if (allChars === '') {
+        alert('Please select at least one character type!');
+        return;
+    }
+
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * allChars.length);
+        password += allChars[randomIndex];
+    }
+
+    passwordDisplay.value = password;
+});
